20b0b7f3b614f49138fe641b67cafae45c5b2baa
SUAN4423 <toyanonichan@yahoo.co.jp>
2019-11-13 14:50:21 +0900

    init

---
+++ FamiVSTi/myvst3fuid.h
@@ -0,0 +1,13 @@
+#pragma once
+// VST3 SDKのインクルードファイル
+#include "pluginterfaces\base\funknown.h"
+
+// VST3作成に必要なの名前空間を使用
+namespace Steinberg {
+	namespace Vst {
+
+		// FUIDの生成
+		static const FUID ProcessorUID(0x837BB17F, 0x50B040A4, 0x80B7BEF4, 0x2FD2F208);
+
+	}
+} // namespace SteinbergとVstの終わり
